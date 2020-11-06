807f4e373e4bb870a2a07295b0555750863dec15
busra <sbusrasubasi@gmail.com>
2020-11-06 13:17:08 +0300

    “Commit”

---
+++ Assets/ARThrow/Scripts/BallControl.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+[RequireComponent(typeof(Rigidbody))]
+public class BallControl : MonoBehaviour
+{
+    public float m_ThrowForce = 100f;
+    public float m_ThrowDirectionX = 0.17f;
+    public float m_ThrowDirectionY = 0.67f;
+    public Vector3 m_BallCameraOffset = new Vector3(0f, -0.4f, 1f);
+
+    private Vector3 startPosition;
+    private Vector3 direction;
+    private float startTime;
+    private float endTime;
+    private float duration;
+    private bool directionChoosen = false;
+    private bool throwStarted = false;
+
+    [SerializeField]
+    GameObject ARCam;
+
+    [SerializeField]
+    ARSessionOrigin m_SessionOrijin;
+
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        m_SessionOrijin = GameObject.Find("AR Session Origin").GetComponent<ARSessionOrigin>();
+        ARCam = m_SessionOrijin.transform.Find("AR Camera").gameObject;
+        transform.parent = ARCam.transform;
+        ResetBall();
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            startPosition = Input.mousePosition;
+            startTime = Time.time;
+            directionChoosen = false;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            endTime = Time.time;
+            duration = endTime - startTime;
+            direction = Input.mousePosition - startPosition;
+            directionChoosen = true;
+        }
+
+        if (directionChoosen)
+        {
+            rb.mass = 1;
+            rb.useGravity = true;
+
+            rb.AddForce(
+                ARCam.transform.forward * m_ThrowForce / duration +
+                ARCam.transform.up * direction.y * m_ThrowDirectionY +
+                ARCam.transform.right * direction.x * m_ThrowDirectionX
+                );
+
+            throwStarted = false;
+            directionChoosen = false;
+            startTime = 0.0f;
+            duration = 0.0f;
+            startPosition = new Vector3(0, 0, 0);
+            direction = new Vector3(0,0,0);
+
+            if (Time.time  - endTime >= 5 && Time.time - endTime <=6)
+            {
+                ResetBall();
+            }
+        }
+    }
+    private void ResetBall()
+    {
+        rb.mass = 0;
+        rb.useGravity = false;
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+        endTime = 0.0f;
+
+        Vector3 ballPoss = ARCam.transform.position + ARCam.transform.forward * m_BallCameraOffset.z + ARCam.transform.up * m_BallCameraOffset.y;
+        transform.position = ballPoss;
+
+    }
+}
